181096cfa5f6157730304cce6e173bbad2d53687
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2019-01-13 15:00:55 +0300

    Init

---
+++ AloeStackView.Samples/Main.cs
@@ -0,0 +1,23 @@
+﻿//
+// Main.cs
+//
+// Author:
+//       Yauheni Pakala <evgeniy.pakalo@gmail.com>
+//
+// Copyright (c) 2019 Yauheni Pakala
+//
+using UIKit;
+
+namespace AloeStackView.Samples
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
---
+++ AloeStackView.Samples/ViewController.cs
@@ -0,0 +1,34 @@
+﻿//
+// ViewController.cs
+//
+// Author:
+//       Yauheni Pakala <evgeniy.pakalo@gmail.com>
+//
+// Copyright (c) 2019 Yauheni Pakala
+//
+using System;
+
+using UIKit;
+
+namespace AloeStackView.Samples
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
